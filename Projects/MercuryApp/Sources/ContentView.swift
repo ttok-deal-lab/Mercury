aa5cefbdea994429cbfa3b86041a567184ef5e5f
Hamin Song <cec7378@gmail.com>
2024-08-03 15:55:17 +0900

    chore: Test 타겟에 mock file 추가

--- Projects/MercuryApp/Sources/ContentView.swift
+++ Projects/MercuryApp/Sources/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-  var body: some View { // View 전처리 또는 의존성 주입 등 ex) onAppear, onDisAppear
+  var body: some View {
     VStack {
       
     }
