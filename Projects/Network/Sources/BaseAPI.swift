c12a9294e5421637089d04b34d573f70e9e59c20
SooHoon <dkehskeh@gmail.com>
2024-11-09 22:31:52 +0900

    feat: 쿼리파라미터 타입 변경/ 함수에 쿼리파람 추가

--- Projects/Network/Sources/BaseAPI.swift
+++ Projects/Network/Sources/BaseAPI.swift
@@ -14,7 +14,7 @@ public protocol BaseAPI {
   var method: HTTPMethod { get }
   var headers: [String: String]? { get }
   var requestBody: [String: Any]? { get }
-  var queryParam: [String: Any]? { get }
+  var queryParam: [URLQueryItem]? { get }
   
   func requestData() async throws -> Data?
   func request<T: Decodable>(_ model: T.Type) async throws -> T where T: Decodable
@@ -32,10 +32,10 @@ public extension BaseAPI {
   var requestBody: [String: Any]? {
     return nil
   }
-    
-    var queryParam: [String : Any]? {
-        return nil
-    }
+  
+  var queryParam: [URLQueryItem]? {
+    return nil
+  }
   
   func requestData() async throws -> Data? {
     var plainURLString = ""
@@ -45,10 +45,16 @@ public extension BaseAPI {
       plainURLString = baseURL.appending(path)
     }
     
-    guard let url: URL = URL(string: plainURLString) else {
+    guard var urlComponents = URLComponents(string: plainURLString) else {
       throw NetworkError.failToConvertURL
     }
-    var urlRequest = URLRequest(url: url, cachePolicy: Const.cachePolicy, timeoutInterval: Const.timeout)
+    urlComponents.queryItems = queryParam
+    
+    guard let finalURL = urlComponents.url else {
+      throw NetworkError.failToConvertURL
+    }
+    
+    var urlRequest = URLRequest(url: finalURL, cachePolicy: Const.cachePolicy, timeoutInterval: Const.timeout)
     
     urlRequest.httpMethod = method.rawValue
     
@@ -62,7 +68,7 @@ public extension BaseAPI {
     }
     
     let (data, _) = try await URLSession.shared.data(for: urlRequest)
-    print("Network request: URL:: \(url.absoluteString), response: \(data)")
+    print("Network request: URL:: \(finalURL.absoluteString), response: \(data)")
     return data
   }
   
@@ -74,16 +80,16 @@ public extension BaseAPI {
       plainURLString = baseURL.appending(path)
     }
     
-      
-      print("plainURLString :", plainURLString)
-    guard let url: URL = URL(string: plainURLString) else {
+    guard var urlComponents = URLComponents(string: plainURLString) else {
       throw NetworkError.failToConvertURL
     }
-      
-    print("url : ", url)
-    var urlRequest = URLRequest(url: url, cachePolicy: Const.cachePolicy, timeoutInterval: Const.timeout)
-      
-      print("urlRequest : ", urlRequest)
+    urlComponents.queryItems = queryParam
+    
+    guard let finalURL = urlComponents.url else {
+      throw NetworkError.failToConvertURL
+    }
+    
+    var urlRequest = URLRequest(url: finalURL, cachePolicy: Const.cachePolicy, timeoutInterval: Const.timeout)
     
     urlRequest.httpMethod = method.rawValue
     
@@ -92,64 +98,14 @@ public extension BaseAPI {
       urlRequest.httpBody = bodyData
     }
     
-      print("requestBody :", requestBody)
-    
     if let headers {
       urlRequest.allHTTPHeaderFields = headers
     }
     
     let (data, _) = try await URLSession.shared.data(for: urlRequest)
-      print("data : ", data)
     let decodedObj = try JSONDecoder().decode(T.self, from: data)
-    print("Network request: URL:: \(url.absoluteString), response: \(decodedObj)")
+    print("Network request: URL:: \(finalURL.absoluteString), response: \(decodedObj)")
     return decodedObj
   }
-    
-    
-    func requestDataWithParam() async throws -> Data? {
-           var plainURLString = ""
-           if let domain = domain {
-               plainURLString = baseURL.appending(domain).appending(path)
-           } else {
-               plainURLString = baseURL.appending(path)
-           }
-         
-           guard var urlComponents = URLComponents(string: plainURLString) else {
-               throw NetworkError.failToConvertURL
-           }
-         
-           // queryParam을 URL의 쿼리 파라미터로 추가
-           if let queryParam = queryParam {
-               urlComponents.queryItems = queryParam.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
-           }
-         
-           guard let finalURL = urlComponents.url else {
-               throw NetworkError.failToConvertURL
-           }
-
-           var urlRequest = URLRequest(url: finalURL, cachePolicy: Const.cachePolicy, timeoutInterval: Const.timeout)
-           urlRequest.httpMethod = method.rawValue
-    
-           if let headers = headers {
-               urlRequest.allHTTPHeaderFields = headers
-           }
-         
-            
-           let (data, _) = try await URLSession.shared.data(for: urlRequest)
-           print("Network request: URL:: \(finalURL.absoluteString), response: \(data)")
-           return data
-       }
-     
-       func requestWithParm<T: Decodable>(_ model: T.Type) async throws -> T {
-           
-           let data = try await requestDataWithParam()
-           
-           guard let data = data else { throw
-               NetworkError.failToConvertURL }
-           let decodedObj = try JSONDecoder().decode(T.self, from: data)
-           
-           return decodedObj
-           
-       }
+  
 }
-
