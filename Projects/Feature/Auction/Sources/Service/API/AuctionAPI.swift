5631784f98fcc491355f51da51e9f60e96f24227
SooHoon <dkehskeh@gmail.com>
2024-11-09 19:10:49 +0900

    feat: BASEAPI에 쿼리파람 리퀘스트 추가

--- Projects/Feature/Auction/Sources/Service/API/AuctionAPI.swift
+++ Projects/Feature/Auction/Sources/Service/API/AuctionAPI.swift
@@ -10,29 +10,48 @@ import Foundation
 import Network
 
 public enum AuctionAPI: BaseAPI {
-    case auctionList
-    case auctionDetail(_ salesId: String)
+    case auctionList(_ largeCategory: String, mediumCategory: String, page: Int)
+    case auctionDetail(_ salesId: String, _ largeCategory: String, mediumCategory: String, _ courtName: String, salesNumber: Int)
+    
     public var baseURL: String {
         "http://sherbet-api.dev.pursue503.com/v1/"
     }
     
-    public var domain: String {
+    public var domain: String? {
         switch self {
-        case.auctionList: "sales/"
-        case .auctionDetail(_): "sales/"
+        case.auctionList, .auctionDetail(_, _, _, _, _): "sales/"
         }
     }
+
     
     public var path: String {
         switch self {
         case .auctionList: "court"
-        case .auctionDetail(let salesId): "court/\(salesId)"
+        case .auctionDetail(let salesId, _, _, _, _): "court/\(salesId)"
         }
     }
     
     public var method: Network.HTTPMethod {
         switch self {
         case .auctionList, .auctionDetail: .get
+        }
+    }
+    
+    public var queryParam: [String : Any]? {
+        switch self {
+        case .auctionList(let largeCategory, let mediumCategory, let page):
+            return [
+                "largeCategory": largeCategory,
+                "mediumCategory": mediumCategory,
+                "page": page
+            ]
+        case .auctionDetail(_, let largeCategory, let mediumCategory, let courtName, let salesNumber):
+            return [
+                "largeCategory": largeCategory,
+                "mediumCategory": mediumCategory,
+                "courtName": courtName,
+                "salesNumber": salesNumber
+            ]
         }
     }
 
--- Projects/Network/Sources/BaseAPI.swift
+++ Projects/Network/Sources/BaseAPI.swift
@@ -14,6 +14,7 @@ public protocol BaseAPI {
   var method: HTTPMethod { get }
   var headers: [String: String]? { get }
   var requestBody: [String: Any]? { get }
+  var queryParam: [String: Any]? { get }
   
   func requestData() async throws -> Data?
   func request<T: Decodable>(_ model: T.Type) async throws -> T where T: Decodable
@@ -31,6 +32,10 @@ public extension BaseAPI {
   var requestBody: [String: Any]? {
     return nil
   }
+    
+    var queryParam: [String : Any]? {
+        return nil
+    }
   
   func requestData() async throws -> Data? {
     var plainURLString = ""
@@ -69,10 +74,16 @@ public extension BaseAPI {
       plainURLString = baseURL.appending(path)
     }
     
+      
+      print("plainURLString :", plainURLString)
     guard let url: URL = URL(string: plainURLString) else {
       throw NetworkError.failToConvertURL
     }
+      
+    print("url : ", url)
     var urlRequest = URLRequest(url: url, cachePolicy: Const.cachePolicy, timeoutInterval: Const.timeout)
+      
+      print("urlRequest : ", urlRequest)
     
     urlRequest.httpMethod = method.rawValue
     
@@ -81,14 +92,64 @@ public extension BaseAPI {
       urlRequest.httpBody = bodyData
     }
     
+      print("requestBody :", requestBody)
+    
     if let headers {
       urlRequest.allHTTPHeaderFields = headers
     }
     
     let (data, _) = try await URLSession.shared.data(for: urlRequest)
+      print("data : ", data)
     let decodedObj = try JSONDecoder().decode(T.self, from: data)
     print("Network request: URL:: \(url.absoluteString), response: \(decodedObj)")
     return decodedObj
   }
+    
+    
+    func requestDataWithParam() async throws -> Data? {
+           var plainURLString = ""
+           if let domain = domain {
+               plainURLString = baseURL.appending(domain).appending(path)
+           } else {
+               plainURLString = baseURL.appending(path)
+           }
+         
+           guard var urlComponents = URLComponents(string: plainURLString) else {
+               throw NetworkError.failToConvertURL
+           }
+         
+           // queryParam을 URL의 쿼리 파라미터로 추가
+           if let queryParam = queryParam {
+               urlComponents.queryItems = queryParam.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
+           }
+         
+           guard let finalURL = urlComponents.url else {
+               throw NetworkError.failToConvertURL
+           }
+
+           var urlRequest = URLRequest(url: finalURL, cachePolicy: Const.cachePolicy, timeoutInterval: Const.timeout)
+           urlRequest.httpMethod = method.rawValue
+    
+           if let headers = headers {
+               urlRequest.allHTTPHeaderFields = headers
+           }
+         
+            
+           let (data, _) = try await URLSession.shared.data(for: urlRequest)
+           print("Network request: URL:: \(finalURL.absoluteString), response: \(data)")
+           return data
+       }
+     
+       func requestWithParm<T: Decodable>(_ model: T.Type) async throws -> T {
+           
+           let data = try await requestDataWithParam()
+           
+           guard let data = data else { throw
+               NetworkError.failToConvertURL }
+           let decodedObj = try JSONDecoder().decode(T.self, from: data)
+           
+           return decodedObj
+           
+       }
 }
 
