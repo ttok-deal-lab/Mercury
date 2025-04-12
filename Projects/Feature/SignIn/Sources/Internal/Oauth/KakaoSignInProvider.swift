dd83336c2068fd8e7656dc33b82cbc261315c802
Hamin Song <cec7378@gmail.com>
2025-04-12 14:51:39 +0900

    refactor: login 구현 방식 리팩토링

--- Projects/Feature/SignIn/Sources/Internal/Oauth/KakaoSignInProvider.swift
+++ Projects/Feature/SignIn/Sources/Internal/Oauth/KakaoSignInProvider.swift
@@ -17,40 +17,44 @@ import KakaoSDKAuth
 
 
 class KakaoSignInProvider: NSObject, OauthSignInable {
+  // MARK: - private method
+  private func kakoTalkLogin(continuation: CheckedContinuation<OauthSignInToken, any Error>) {
+    UserApi.shared.loginWithKakaoTalk { (oauthToken, error) in
+      if let error = error {
+        let mercuryError = MercuryError(code: (error as NSError).code)
+        continuation.resume(throwing: mercuryError)
+        return
+      }
+      guard let token = oauthToken?.idToken else {
+        continuation.resume(throwing: MercuryError(from: .ownModule(.kakaoSignin), .unknown))
+        return
+      }
+      continuation.resume(returning: token)
+    }
+  }
+  
+  private func kakaoAccountLogin(continuation: CheckedContinuation<OauthSignInToken, any Error>) {
+    UserApi.shared.loginWithKakaoAccount { (oauthToken, error) in
+      if let error = error {
+        let mercuryError = MercuryError(code: (error as NSError).code)
+        continuation.resume(throwing: mercuryError)
+        return
+      }
+      guard let token = oauthToken?.idToken else {
+        continuation.resume(throwing: MercuryError(from: .ownModule(.kakaoSignin), .unknown))
+        return
+      }
+      continuation.resume(returning: token)
+    }
+  }
+  
   
   // MARK: - internal method
   
   @MainActor
   func signIn() async throws -> OauthSignInToken {
-    
     return try await withCheckedThrowingContinuation { continuation in
-      if (UserApi.isKakaoTalkLoginAvailable()) { // 카카오톡으로 로그인
-        UserApi.shared.loginWithKakaoTalk { (oauthToken, error) in
-          if let error = error {
-            let mercuryError = MercuryError(code: (error as NSError).code)
-            continuation.resume(throwing: mercuryError)
-            return
-          }
-          guard let token = oauthToken?.idToken else {
-            continuation.resume(throwing: MercuryError(from: .ownModule(.kakaoSignin), .unknown))
-            return
-          }
-          continuation.resume(returning: token)
-        }
-      } else { // WebView 띄워서 카카오 계정으로 로그인
-        UserApi.shared.loginWithKakaoAccount { (oauthToken, error) in
-          if let error = error {
-            let mercuryError = MercuryError(code: (error as NSError).code)
-            continuation.resume(throwing: mercuryError)
-            return
-          }
-          guard let token = oauthToken?.idToken else {
-            continuation.resume(throwing: MercuryError(from: .ownModule(.kakaoSignin), .unknown))
-            return
-          }
-          continuation.resume(returning: token)
-        }
-      }
+      UserApi.isKakaoTalkLoginAvailable() ? kakoTalkLogin(continuation: continuation) : kakaoAccountLogin(continuation: continuation)
     }
   }
 }
