4f78c882c60d9af21fef735bff909ea2ef62dd9a
SooHoon <dkehskeh@gmail.com>
2025-05-04 19:43:28 +0900

    fix: KakaoSignIn 토큰 값 수정
    idToken -> accessToken

--- Projects/Feature/Onboard/Sources/Internal/Oauth/KakaoSignInProvider.swift
+++ Projects/Feature/Onboard/Sources/Internal/Oauth/KakaoSignInProvider.swift
@@ -26,7 +26,7 @@ class KakaoSignInProvider: OauthSignInable {
         continuation.resume(throwing: mercuryError)
         return
       }
-      guard let token = oauthToken?.idToken else {
+      guard let token = oauthToken?.accessToken else {
         continuation.resume(throwing: MercuryError(.noOauthToken))
         return
       }
@@ -41,7 +41,7 @@ class KakaoSignInProvider: OauthSignInable {
         continuation.resume(throwing: MercuryError(code: (error as NSError).code))
         return
       }
-      guard let token = oauthToken?.idToken else {
+      guard let token = oauthToken?.accessToken else {
         continuation.resume(throwing: MercuryError(.noOauthToken))
         return
       }
