922c4a6b6d5233954f1f2c035df0dfbec6377cc7
SooHoon <dkehskeh@gmail.com>
2025-12-21 21:28:28 +0900

    WIP: 코드정리

--- Projects/AppFoundation/Sources/Define/PolicyURL.swift
+++ Projects/AppFoundation/Sources/Define/PolicyURL.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public enum PolicyURL {
-    public static let memberAgreement = "https://www.notion.so/v1-2ba5ff9f915c80f4b458ef4d48083820?source=copy_link"
-    public static let privacyPolicy = "https://www.notion.so/iOS-ver-1-2ba5ff9f915c80ad9e59e6a365337e52?source=copy_link"
-    public static let servicePolicy = "https://www.notion.so/ver-1-2ba5ff9f915c807d84e1d65fa44177e1?source=copy_link"
+  public static let memberAgreement = "https://www.notion.so/v1-2ba5ff9f915c80f4b458ef4d48083820?source=copy_link"
+  public static let privacyPolicy = "https://www.notion.so/iOS-ver-1-2ba5ff9f915c80ad9e59e6a365337e52?source=copy_link"
+  public static let servicePolicy = "https://www.notion.so/ver-1-2ba5ff9f915c807d84e1d65fa44177e1?source=copy_link"
 }
