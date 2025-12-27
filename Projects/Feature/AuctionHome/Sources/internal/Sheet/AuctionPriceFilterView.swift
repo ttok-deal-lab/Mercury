b4b21f56e480765b95bc5f5c5c9c06e70a62582b
Hamin Song <cec7378@gmail.com>
2025-12-27 23:29:29 +0900

    feat: 경매 가격 필터링

--- Projects/Feature/AuctionHome/Sources/internal/Sheet/AuctionPriceFilterView.swift
+++ Projects/Feature/AuctionHome/Sources/internal/Sheet/AuctionPriceFilterView.swift
@@ -11,9 +11,9 @@ import UIComponent
 
 struct AuctionPriceFilterView: View {
   @Environment(AuctionHomeModelData.self) var modelData
-  @State private var selectedChipTitles: Set<String> = []
-  @State var lowestPrice: Double = .zero
-  @State var highestPrice: Double = 20_000
+  @State private var priceRange: ClosedRange<Double> = 0...2_000_000_000
+  @State private var isFirstEnter = true
+  
   var onComplete: () -> Void
   
   var body: some View {
@@ -27,18 +27,28 @@ struct AuctionPriceFilterView: View {
       .padding(.vertical, 16)
       .padding(.horizontal, 20)
       
-      RangeSlider(
-        lowerValue: $lowestPrice,
-        upperValue: $highestPrice,
-        range: 0...20000
+      PriceRangeSlider(
+        range: $priceRange,
+        bounds: 0...2_000_000_000
       )
-        .padding(.horizontal, 20)
       
       MercuryButton("\(modelData.filteredItemCount)개 매물 보기") {
         onComplete()
       }
       .padding(.horizontal, 20)
       .padding(.vertical, 12)
+    }
+    .task(id: priceRange) {
+      do {
+        if isFirstEnter {
+          isFirstEnter = false
+          return
+        }
+        try await Task.sleep(nanoseconds: 500_000_000)
+        modelData.currentAuctionFilter.minimumPrice = Int(priceRange.lowerBound)
+        modelData.currentAuctionFilter.maximumPrice = Int(priceRange.upperBound)
+        await modelData.loadAuctionSalesList()
+      } catch { }
     }
     
   }
