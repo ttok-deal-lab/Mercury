e93b235168eaed2067fa170fa60dfad0bfa7847b
SooHoon95 <dkehskeh@gmail.com>
2026-01-07 22:50:52 +0900

    WIP: 코드 정리

--- Projects/Feature/Setting/Sources/internal/Model/LicenseItem.swift
+++ Projects/Feature/Setting/Sources/internal/Model/LicenseItem.swift
@@ -5,25 +5,33 @@
 //  Created by 최수훈 on 1/7/26.
 //
 
-// 목록 파일용
 struct LicenseRoot: Decodable {
-    let preferenceSpecifiers: [LicenseSpecifier]
-    enum CodingKeys: String, CodingKey { case preferenceSpecifiers = "PreferenceSpecifiers" }
+  let preferenceSpecifiers: [LicenseSpecifier]
+  enum CodingKeys: String, CodingKey {
+    case preferenceSpecifiers = "PreferenceSpecifiers"
+  }
 }
 
 struct LicenseSpecifier: Decodable {
-    let title: String?
-    let file: String?
-    enum CodingKeys: String, CodingKey { case title = "Title"; case file = "File" }
+  let title: String?
+  let file: String?
+  enum CodingKeys: String, CodingKey {
+    case title = "Title"
+    case file = "File"
+  }
 }
 
-// 상세 파일용
+// 상세
 struct LicenseDetailRoot: Decodable {
-    let preferenceSpecifiers: [LicenseDetailSpecifier]
-    enum CodingKeys: String, CodingKey { case preferenceSpecifiers = "PreferenceSpecifiers" }
+  let preferenceSpecifiers: [LicenseDetailSpecifier]
+  enum CodingKeys: String, CodingKey {
+    case preferenceSpecifiers = "PreferenceSpecifiers"
+  }
 }
 
 struct LicenseDetailSpecifier: Decodable {
-    let footerText: String?
-    enum CodingKeys: String, CodingKey { case footerText = "FooterText" }
+  let footerText: String?
+  enum CodingKeys: String, CodingKey {
+    case footerText = "FooterText"
+  }
 }
